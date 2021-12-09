f39b6eb1d087fb8473f22d0bcba2f8274969e72f
0andme <yu00mi97@gmail.com>
2021-12-09 21:08:43 +0900

    feat : 상품 검색 api 호출

---
+++ server/routes/searchItem.js
@@ -0,0 +1,33 @@
+var express = require("express");
+require("dotenv").config();
+var router = express.Router();
+const bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(express.json());
+
+const client_id = process.env.NAVER_CLIENT_ID;
+const client_secret = process.env.NAVER_CLIENT_SECRET;
+
+router.post("/", function (req, res, next) {
+  const api_url = `https://openapi.naver.com/v1/search/shop.json?query=${encodeURI(
+    req.body.query
+  )}`;
+  const request = require("request");
+  const options = {
+    url: api_url,
+    headers: {
+      "X-Naver-Client-Id": client_id,
+      "X-Naver-Client-Secret": client_secret,
+    },
+  };
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
+      res.end(body);
+    } else {
+      res.status(response.statusCode).end();
+      console.log("error = " + response.statusCode);
+    }
+  });
+});
+module.exports = router;
