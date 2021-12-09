596c11aca7c638c7583e4fd940b224f8345d6d40
0andme <yu00mi97@gmail.com>
2021-12-09 19:28:22 +0900

    feat : home 컴포넌트 생성
    state: 검색어 자동완성 기능을 통해 선택된 data

---
+++ client/src/components/Home.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Container } from "reactstrap";
+
+function Home() {
+  const [selectedData, setSelectedData] = useState("");
+  return <Container className="main"></Container>;
+}
+
+export default Home;
