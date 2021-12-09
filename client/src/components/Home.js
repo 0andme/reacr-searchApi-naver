2c9a3e9b3e8e0e1afc982067d5d5a4ddc6da635d
0andme <yu00mi97@gmail.com>
2021-12-09 20:28:06 +0900

    feat : 상품 검색 input 컴포넌트 SearchBar생성
    state
    -searchKeyWords :  api에서 호출받아온 input 데이터의 연관 검색어목록들
    -isSelectItem : 연관 검색어 목록 중 item이 선택되면 true,input창이 focus되면 false
    
    props
    -setSelectedData : 상위 컴포넌트 Home 으로부터 하위 컴포넌트 SearchItem으로 전달만
    
    function
    -input의 onchane 이벤트 시 검색어 자동완성 api호출

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
+import SearchBar from "./SearchBar";
 import { Container } from "reactstrap";
 
 function Home() {
   const [selectedData, setSelectedData] = useState("");
-  return <Container className="main"></Container>;
+  return (
+    <Container className="main">
+      <SearchBar setSelectedData={setSelectedData} />
+    </Container>
+  );
 }
 
 export default Home;
---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import axios from "axios";
+import SearchItem from "./SearchItem";
+import { Card, Input, CardHeader, ListGroup } from "reactstrap";
+function SearchBar(props) {
+  const [searchKeyWords, setSearchKeyWords] = useState([]);
+  const [isSelectItem, setIsSelectItem] = useState(false);
+  // home으로부터 SearchItem로 전달만
+  const { setSelectedData } = props;
+  return (
+    <>
+      {/* <Card body className="text-right"> */}
+      <CardHeader tag="h5">상품 검색</CardHeader>
+      <Input
+        id="searchBar"
+        placeholder="검색어를 입력하세요"
+        onFocus={resetIsSelectItem}
+        onChange={getSearchRes}
+      />
+      {/* </Card> */}
+
+      {!isSelectItem && searchKeyWords.length > 0 && (
+        <ListGroup>
+          {searchKeyWords.map((item, index) => (
+            <SearchItem
+              setSelectedData={setSelectedData}
+              setIsSelectItem={setIsSelectItem}
+              searchedItem={item}
+              key={index}
+            />
+          ))}
+        </ListGroup>
+      )}
+    </>
+  );
+  // input 창 focus시 isSelectItem state false로 reset
+  function resetIsSelectItem() {
+    setIsSelectItem(false);
+  }
+  // 검색어 자동완성 api호출
+  function getSearchRes(userInput) {
+    axios
+      .post("/search", { query: userInput.target.value })
+      .then((res) => {
+        setSearchKeyWords(res.data.items[0]);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }
+}
+
+export default SearchBar;
