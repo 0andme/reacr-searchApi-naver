ba22e78ddd3bc6924a0e4d2f4491adf269f10b16
0andme <yu00mi97@gmail.com>
2021-12-16 15:12:39 +0900

    feat : DB에서 카테고리별로 데이터 가져오기
    :select
    :selectProductStats

---
+++ server/routes/getChart.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var router = express.Router();
+
+const bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(express.json());
+
+router.post("/", function (req, res, next) {
+  const type = req.body.type;
+  if (type === "getChart") {
+    try {
+      //// Mysql Api 모듈 (CRUD)
+      const dbconnect_Module = require("./dbconnect_Module");
+      //// Mysql 쿼리 호출 정보 입력
+      req.body.mapper = "NaverProductMapper"; // mybatis xml 파일명
+      req.body.crud = "select";
+      req.body.mapper_id = "selectProductStats";
+      router.use("/", dbconnect_Module);
+      next("route");
+    } catch (error) {
+      console.log("Module > dbconnect error: ", error);
+      res.send("error");
+    }
+  } else if (type === "category") {
+    try {
+      //   // Mysql Api 모듈 (CRUD)
+      const dbconnect_Module = require("./dbconnect_Module");
+      //   // Mysql 쿼리 호출 정보 입력
+      req.body.mapper = "NaverProductMapper"; // mybatis xml 파일명
+      req.body.crud = "select";
+      req.body.mapper_id = "selectProductCategory";
+      router.use("/", dbconnect_Module);
+      next("route");
+    } catch (error) {
+      console.log("Module > dbconnect error: ", error);
+      res.send("error");
+    }
+  }
+});
+
+module.exports = router;
