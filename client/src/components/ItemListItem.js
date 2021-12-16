0cf7954c65383d12d0a123d36e519d1c3977586e
0andme <yu00mi97@gmail.com>
2021-12-16 15:21:55 +0900

    feat : 구매 버튼 클릭시 db에 아이템 저장
    function funSaveItem
    : 클릭된 아이템의 정보를 post 방식으로 넘겨준다

--- client/src/components/ItemListItem.js
+++ client/src/components/ItemListItem.js
@@ -1,11 +1,11 @@
 import { Button } from "reactstrap";
-
+import axios from "axios";
 function ItemListItem(props) {
   const { item } = props;
   return (
     <tr className="itemList__item">
       <td>
-        <img src={item.image} alt="" />
+        <img src={item.image} alt={item.title} />
       </td>
       <td>
         <a href={item.link}>{item.title.replace(/([<b>]|[</b>])/g, "")}</a>
@@ -18,9 +18,28 @@ function ItemListItem(props) {
         <span>{item.mallName}</span>
       </td>
       <td>
-        <Button>button</Button>
+        <Button onClick={funSaveItem}>구매</Button>
       </td>
     </tr>
   );
+  function funSaveItem() {
+    axios
+      .post("/save", {
+        title: item.title,
+        image: item.image,
+        productId: item.productId,
+        category1: item.category1,
+        category2: item.category2,
+        category3: item.category3,
+        category4: item.category4,
+        buyCount: 1,
+      })
+      .then((res) => {
+        alert("구매 완료!");
+      })
+      .catch((error) => {
+        alert("구매 중 오류가 발생했습니다.", error, "error", "닫기");
+      });
+  }
 }
 export default ItemListItem;
