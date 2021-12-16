c9595b1b0c902d99913ce617b9969926701a1931
0andme <yu00mi97@gmail.com>
2021-12-16 15:18:48 +0900

    feat : 라우터 추가
    - save: 상품 구매
    - chart : 카테고리별 상품 조회

--- server/server.js
+++ server/server.js
@@ -3,11 +3,13 @@ var express = require("express");
 var indexRouter = require("./routes/index");
 var searchRouter = require("./routes/searchKeyword");
 var shopRouter = require("./routes/searchItem");
-
+var ChartRouter = require("./routes/getChart");
+var SaveItem = require("./routes/saveItem");
 var app = express();
 
 app.use("/", indexRouter);
 app.use("/search", searchRouter);
 app.use("/shop", shopRouter);
-
+app.use("/chart", ChartRouter);
+app.use("/save", SaveItem);
 module.exports = app;
