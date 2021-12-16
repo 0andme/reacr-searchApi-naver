cdb7c48e55dbef35e83f719be43b573bb8292293
0andme <yu00mi97@gmail.com>
2021-12-16 15:59:03 +0900

    feat : Chart 컴포넌트 추가
    state
    -stats : 차트 내 사용된 데이터 : db로부터 받아옴
    -categoryName : 차트에서 클릭된 카테고리 명을 저장
    
    useEffect
    : funcSelectStats을 실행
    : 화면 첫 렌더 이후 한번만 실행하도록 옵션지정 []
    
    function funcSelectStats
    : chart에 사용할 stats를 db로부터 가져온다
    
    function fncOnClick
    : 클릭된 차트 카테고리로 state를 변경한다

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,7 +1,82 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Button, Col, Container, Row } from "reactstrap";
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from "recharts";
+import CategoryItems from "./CategoryItems";
 
 const Chart = () => {
-  return <div className="main">About</div>;
+  const [stats, setStat] = useState([]);
+  const [categoryName, setCategoryName] = useState("");
+  useEffect(() => {
+    funcSelectStats();
+  }, []);
+
+  return (
+    <div className="main">
+      <>
+        <Row>
+          <Col>
+            <Container style={{ paddingTop: "30px" }}>
+              <Row>
+                <Col style={{ textAlign: "right" }}>
+                  <Button onClick={funcSelectStats}>조회</Button>
+                </Col>
+              </Row>
+              <Row>
+                <LineChart
+                  width={1000}
+                  height={300}
+                  data={stats}
+                  onClick={fncOnClick}
+                  margin={{
+                    top: 5,
+                    right: 50,
+                    left: 0,
+                    bottom: 5,
+                  }}
+                >
+                  {/* <CartesianGrid strokeDasharray="3 3" /> */}
+
+                  <XAxis dataKey="category1" />
+
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Line
+                    type="monotone"
+                    dataKey="buyCount"
+                    stroke="#8884d8"
+                    activeDot={{ r: 8 }}
+                  />
+                </LineChart>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </>
+      {categoryName && <CategoryItems categoryName={categoryName} />}
+    </div>
+  );
+  function funcSelectStats() {
+    axios
+      .post("/chart", { type: "getChart" })
+      .then((response) => {
+        if (response.data.json) {
+          const results = response.data.json;
+          if (results.length > 0) {
+            setStat(results);
+          }
+        }
+      })
+      .catch();
+  }
+  function fncOnClick(e) {
+    if (e === null) {
+      return;
+    } else if (e.activeLabel !== null) {
+      setCategoryName(e.activeLabel);
+    }
+  }
 };
 
 export default Chart;
